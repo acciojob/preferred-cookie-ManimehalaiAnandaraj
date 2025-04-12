5407f6b31b0378fb56503c6b144ba8d1dacac48c
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-12 12:11:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-//your JS code here. If required.
-function setCookie(name,value,days) {
-	var expires="";
-	if(days)
-	{
-		var date=new Date();
-		date.setTime(date.getTime()+(days * 24 *60*60*1000));
-		expires=";expires=" + date.toUTCString();
-	}
-	document.cookie=name + "=" + (value || "") +expires + +"; path=/";
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
 // Function to get a cookie
